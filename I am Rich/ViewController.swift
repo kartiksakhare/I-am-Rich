e0d4b7b53603020ee82bc2405bebf1cc7e0834bb
kartiksakhare <kartiksakhare333@gmail.com>
2022-10-30 10:04:07 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kartik Sakhare on 30/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
